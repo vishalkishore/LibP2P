922f7ba2db6a2f08479a63c4d92b8e81e6612aa5
Vishal Kishore <kishorevishal01@gmail.com>
2024-02-02 15:56:06 +0530

    made modular code

--- test.server.go
+++ test.server.go
@@ -1,93 +1,79 @@
-
 package main
 
 import (
-	"fmt"
-	"log"
-	"os"
-	// "context"
-	"os/signal"
-	"syscall"
-	libp2p "github.com/libp2p/go-libp2p"
-	peerstore "github.com/libp2p/go-libp2p/core/peer"
-	"github.com/libp2p/go-libp2p/core/host"
-	"github.com/libp2p/go-libp2p/core/protocol"
-	"github.com/libp2p/go-libp2p/core/network"
+    "fmt"
+    "log"
+    "os"
+    "os/signal"
+    "syscall"
 
-	// "github.com/multiformats/go-multiaddr"
-	// "github.com/vishalkishore/p2pFS"
-	"github.com/libp2p/go-libp2p-core/crypto"
+    libp2p "github.com/libp2p/go-libp2p"
+    "github.com/libp2p/go-libp2p/core/crypto"
+    "github.com/libp2p/go-libp2p/core/host"
+    "github.com/libp2p/go-libp2p/core/network"
+    "github.com/libp2p/go-libp2p/core/peer"
+    "github.com/libp2p/go-libp2p/core/protocol"
 )
 
 func main() {
-	node := initNode()
-	
-	peerInfo := GetPeerInfo(node)
+    node := initNode()
+    defer node.Close()
 
-	combinedAddr, err := peerstore.AddrInfoToP2pAddrs(&peerInfo)
-	if err != nil {
-		panic(err)
-	}
+    peerInfo := getPeerInfo(node)
+    combinedAddr, err := peer.AddrInfoToP2pAddrs(&peerInfo)
+    if err != nil {
+        panic(err)
+    }
 
-	fmt.Println("libp2p node address:", combinedAddr[0])
+    setStreamHandler(node)
 
-	// Set a stream handler on "/my-protocol"
-    node.SetStreamHandler(protocol.ID("/my-protocol"), func(s network.Stream) {
+    fmt.Println("Server is listening on", combinedAddr[0])
+
+    waitForSignal()
+}
+
+func initNode() host.Host {
+    priv, _, err := crypto.GenerateKeyPair(crypto.RSA, 2048)
+    if err != nil {
+        log.Fatal("Error generating key pair: ", err)
+    }
+
+    node, err := libp2p.New(libp2p.Identity(priv),
+        libp2p.ListenAddrStrings("/ip4/127.0.0.1/tcp/8080"),
+    )
+    if err != nil {
+        panic(err)
+    }
+    return node
+}
+
+func getPeerInfo(node host.Host) peer.AddrInfo {
+    return peer.AddrInfo{
+        ID:    node.ID(),
+        Addrs: node.Addrs(),
+    }
+}
+
+func setStreamHandler(node host.Host) {
+    node.SetStreamHandler(protocol.ID("/file-share"), func(s network.Stream) {
         fmt.Println("New stream opened")
 
-        // Send a message to the client
         _, err := s.Write([]byte("Hello, client!"))
         if err != nil {
             panic(err)
         }
 
-        // Close the stream
         err = s.Close()
         if err != nil {
             panic(err)
         }
     })
-
-    fmt.Println("Server is listening on", node.Addrs())
-
-	WaitForSignal(node)
 }
 
-func initNode() (node host.Host) {
-	// start a libp2p node with default settings
-	// ctx := context.Background()
+func waitForSignal() {
+    ch := make(chan os.Signal, 1)
+    signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+    <-ch
 
-	priv, _, err := crypto.GenerateKeyPair(crypto.RSA, 2048)
-	if err != nil {
-		log.Fatal("Error generating key pair: ", err)
-	}
-
-	node, err = libp2p.New(libp2p.Identity(priv),
-			libp2p.ListenAddrStrings("/ip4/127.0.0.1/tcp/8080"),
-		)
-	if err != nil {
-		panic(err)
-	}
-	return node
-}
-
-func GetPeerInfo(node host.Host) (peerInfo peerstore.AddrInfo) {
-	peerInfo = peerstore.AddrInfo{
-		ID:    node.ID(),
-		Addrs: node.Addrs(),
-	}
-	return peerInfo
-}
-
-func WaitForSignal(node host.Host) {
-	ch := make(chan os.Signal, 1)
-	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
-	<-ch
-
-	fmt.Println("Received signal, shutting down...")
-
-	// shut the node down
-	if err := node.Close(); err != nil {
-		panic(err)
-	}
+    fmt.Println("Received signal, shutting down...")
 }
