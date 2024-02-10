55cc5c10d300a83ada7cce496395fe20928a2cea
kishore-das <vishal.turing@gmail.com>
2024-02-10 12:03:46 +0530

    Refactor client code to support interactive messaging with the server

--- test.client.go
+++ test.client.go
@@ -1,76 +1,114 @@
 package main
 
 import (
-    "context"
-    "fmt"
-    "io/ioutil"
+	"bufio"
+	"context"
+	"fmt"
+	"os"
 
-    libp2p "github.com/libp2p/go-libp2p"
-    "github.com/libp2p/go-libp2p/core/host"
-    "github.com/libp2p/go-libp2p/core/peer"
-    "github.com/libp2p/go-libp2p/core/network"
-    "github.com/libp2p/go-libp2p/core/protocol"
-    ma "github.com/multiformats/go-multiaddr"
+	libp2p "github.com/libp2p/go-libp2p"
+	"github.com/libp2p/go-libp2p/core/host"
+	"github.com/libp2p/go-libp2p/core/network"
+	"github.com/libp2p/go-libp2p/core/peer"
+	"github.com/libp2p/go-libp2p/core/protocol"
+	ma "github.com/multiformats/go-multiaddr"
 )
 
 func main() {
-    ctx := context.Background()
+	ctx := context.Background()
 
-    h := createHost()
-    defer h.Close()
+	h := createHost()
+	defer h.Close()
 
-    info := getServerInfo("/ip4/127.0.0.1/tcp/8080/p2p/QmeBc2W7QSAoFs4gPGVG3hy1gL82ixg1qHBxTYj6Z39RT1")
+	info := getServerInfo("/ip4/127.0.0.1/tcp/8080/p2p/QmadYGN3eoVQD2A7U7pd1wT5St7pFcGcaFUjwZ7ojgqMoz")
 
-    connectToServer(ctx, h, info)
+	connectToServer(ctx, h, info)
+	// if err != nil {
+	// 	fmt.Println("Error connecting to server:", err)
+	// 	return
+	// }
 
-    s := openStream(ctx, h, info, "/file-share")
+	s := openStream(ctx, h, info, "/file-share")
 
-    message := readMessage(s)
+	reader := bufio.NewReader(s)
+	writer := bufio.NewWriter(s)
 
-    fmt.Printf("Received message from server: %s\n", string(message))
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Enter a message to send to the server: ")
+		if !scanner.Scan() {
+			fmt.Println("Error reading input:", scanner.Err())
+			break
+		}
+		message := scanner.Text()
+
+		// Send the message to the server
+		var err error
+		_, err = writer.WriteString(message + "\n")
+		if err != nil {
+			fmt.Println("Error writing to server:", err)
+			break
+		}
+		writer.Flush()
+
+		// Read the server's response
+		response, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Error reading from server:", err)
+			break
+		}
+		fmt.Printf("Received response from server: %s\n", response)
+
+		fmt.Print("Do you want to continue (yes/no)? ")
+		if !scanner.Scan() {
+			fmt.Println("Error reading input:", scanner.Err())
+			break
+		}
+		confirmation := scanner.Text()
+		if confirmation != "yes" {
+			break
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading input:", err)
+	}
 }
 
 func createHost() host.Host {
-    h, err := libp2p.New()
-    if err != nil {
-        panic(err)
-    }
-    return h
+	h, err := libp2p.New()
+	if err != nil {
+		panic(err)
+	}
+	return h
 }
 
 func getServerInfo(maddrStr string) *peer.AddrInfo {
-    maddr, err := ma.NewMultiaddr(maddrStr)
-    if err != nil {
-        panic(err)
-    }
+	maddr, err := ma.NewMultiaddr(maddrStr)
+	if err != nil {
+		panic(err)
+	}
 
-    info, err := peer.AddrInfoFromP2pAddr(maddr)
-    if err != nil {
-        panic(err)
-    }
+	info, err := peer.AddrInfoFromP2pAddr(maddr)
+	if err != nil {
+		panic(err)
+	}
 
-    return info
+	return info
 }
 
 func connectToServer(ctx context.Context, h host.Host, info *peer.AddrInfo) {
-    err := h.Connect(ctx, *info)
-    if err != nil {
-        panic(err)
-    }
+	err := h.Connect(ctx, *info)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func openStream(ctx context.Context, h host.Host, info *peer.AddrInfo, protocolID string) network.Stream {
-    s, err := h.NewStream(ctx, info.ID, protocol.ID(protocolID))
-    if err != nil {
-        panic(err)
-    }
-    return s
-}
-
-func readMessage(s network.Stream) []byte {
-    buf, err := ioutil.ReadAll(s)
-    if err != nil {
-        panic(err)
-    }
-    return buf
+	s, err := h.NewStream(ctx, info.ID, protocol.ID(protocolID))
+	if err != nil {
+		panic(err)
+	}
+	return s
 }
--- test.server.go
+++ test.server.go
@@ -1,79 +1,131 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "os"
-    "os/signal"
-    "syscall"
+	"bufio"
+	"context"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
-    libp2p "github.com/libp2p/go-libp2p"
-    "github.com/libp2p/go-libp2p/core/crypto"
-    "github.com/libp2p/go-libp2p/core/host"
-    "github.com/libp2p/go-libp2p/core/network"
-    "github.com/libp2p/go-libp2p/core/peer"
-    "github.com/libp2p/go-libp2p/core/protocol"
+	libp2p "github.com/libp2p/go-libp2p"
+	"github.com/libp2p/go-libp2p/core/crypto"
+	"github.com/libp2p/go-libp2p/core/host"
+	"github.com/libp2p/go-libp2p/core/network"
+	"github.com/libp2p/go-libp2p/core/peer"
+	"github.com/libp2p/go-libp2p/core/protocol"
 )
 
+var listenAddr = flag.String("listen", "/ip4/127.0.0.1/tcp/8080", "The address to listen on")
+
 func main() {
-    node := initNode()
-    defer node.Close()
+	flag.Parse()
 
-    peerInfo := getPeerInfo(node)
-    combinedAddr, err := peer.AddrInfoToP2pAddrs(&peerInfo)
-    if err != nil {
-        panic(err)
-    }
+	node := initNode(*listenAddr)
+	defer node.Close()
 
-    setStreamHandler(node)
+	peerInfo := getPeerInfo(node)
+	combinedAddr, err := peer.AddrInfoToP2pAddrs(&peerInfo)
+	if err != nil {
+		log.Fatalf("Error converting peer info to p2p addrs: %v", err)
+	}
 
-    fmt.Println("Server is listening on", combinedAddr[0])
+	setStreamHandler(node)
 
-    waitForSignal()
+	log.Printf("Server is listening on %s", combinedAddr[0])
+
+	waitForSignal(node)
 }
 
-func initNode() host.Host {
-    priv, _, err := crypto.GenerateKeyPair(crypto.RSA, 2048)
-    if err != nil {
-        log.Fatal("Error generating key pair: ", err)
-    }
+func initNode(listenAddr string) host.Host {
+	priv, _, err := crypto.GenerateKeyPair(crypto.RSA, 2048)
+	if err != nil {
+		log.Fatalf("Error generating key pair: %v", err)
+	}
 
-    node, err := libp2p.New(libp2p.Identity(priv),
-        libp2p.ListenAddrStrings("/ip4/127.0.0.1/tcp/8080"),
-    )
-    if err != nil {
-        panic(err)
-    }
-    return node
+	node, err := libp2p.New(libp2p.Identity(priv),
+		libp2p.ListenAddrStrings(listenAddr),
+	)
+	if err != nil {
+		log.Fatalf("Error creating libp2p node: %v", err)
+	}
+	return node
 }
 
 func getPeerInfo(node host.Host) peer.AddrInfo {
-    return peer.AddrInfo{
-        ID:    node.ID(),
-        Addrs: node.Addrs(),
-    }
+	return peer.AddrInfo{
+		ID:    node.ID(),
+		Addrs: node.Addrs(),
+	}
 }
 
 func setStreamHandler(node host.Host) {
-    node.SetStreamHandler(protocol.ID("/file-share"), func(s network.Stream) {
-        fmt.Println("New stream opened")
+	node.SetStreamHandler(protocol.ID("/file-share"), func(s network.Stream) {
+		fmt.Println("New stream opened")
+		reader := bufio.NewReader(s)
+		writer := bufio.NewWriter(s)
 
-        _, err := s.Write([]byte("Hello, client!"))
-        if err != nil {
-            panic(err)
-        }
+		for {
+			// Read a message from the client
+			message, err := reader.ReadString('\n')
+			if err != nil {
+				if err == io.EOF {
+					fmt.Println("Client closed the connection")
+					break
+				}
+				fmt.Println("Error reading from client:", err)
+				sendError(writer, "Error reading from client")
+				break
+			}
+			fmt.Printf("Received message from client: %s\n", message)
 
-        err = s.Close()
-        if err != nil {
-            panic(err)
-        }
-    })
+			// Respond to the client
+			_, err = writer.WriteString("Hello, client!\n")
+			if err != nil {
+				fmt.Println("Error writing to client:", err)
+				sendError(writer, "Error writing to client")
+				break
+			}
+			writer.Flush()
+		}
+
+		err := s.Close()
+		if err != nil {
+			fmt.Println("Error closing stream:", err)
+		}
+	})
 }
 
-func waitForSignal() {
-    ch := make(chan os.Signal, 1)
-    signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
-    <-ch
+func sendError(writer *bufio.Writer, errorMessage string) {
+	_, err := writer.WriteString(errorMessage + "\n")
+	if err != nil {
+		fmt.Println("Error sending error message:", err)
+	}
+	writer.Flush()
+}
 
-    fmt.Println("Received signal, shutting down...")
+func waitForSignal(node host.Host) {
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+	<-ch
+
+	log.Println("Received signal, shutting down...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := node.Close(); err != nil {
+		log.Fatalf("Error closing libp2p node: %v", err)
+	}
+
+	select {
+	case <-ctx.Done():
+		log.Println("Shutdown gracefully")
+	case <-time.After(6 * time.Second):
+		log.Println("Shutdown forcefully")
+	}
 }
